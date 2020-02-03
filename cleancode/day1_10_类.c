49cb70d1078a6528133b32a93f4c754aaaf9fc97
YangGu <gy3420@163.com>
2020-02-03 14:24:49 +0800

    cleanclode 10章类

--- cleancode/day1_10_类.c
+++ cleancode/day1_10_类.c
@@ -76,6 +76,3 @@ public class Client {
       xiaoLi.drive(benz);
    }
 }
-————————————————
-版权声明：本文为CSDN博主「皮皮的雅客」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/king123456man/article/details/81626127
